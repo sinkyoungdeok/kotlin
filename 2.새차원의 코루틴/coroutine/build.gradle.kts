1e4bf82dc6e3bfb30eae0626a600858c2a34a89f
sinkyoungdeok <tlsrudejr123@nate.com>
2021-12-26 21:20:34 +0900

    [UPDATE] 코루틴 버전문제 해결

--- 2.새차원의 코루틴/coroutine/build.gradle.kts
+++ 2.새차원의 코루틴/coroutine/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.5.10"
+    kotlin("jvm") version "1.6.10"
     application
 }
 
@@ -13,7 +13,7 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.0.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.6")
     testImplementation(kotlin("test"))
 }
 
